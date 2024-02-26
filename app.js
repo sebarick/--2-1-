9235b742e17b4da6587a3b1abee2a8d26ed83e1a
sebarick <157357495+sebarick@users.noreply.github.com>
2024-02-26 11:08:00 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//12 вариант, проверка на простое число
+
+let res1 = () => {
+    console.log("True");
+}
+let res2 = () => {
+    console.log("False");
+}
+let num = ();
+if (num/1 = num, num/num = 1) {
+    res1()
+} else {
+    res2();
+} 
