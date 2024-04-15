285722eb66dcffda32d1c1366057b43c6ee67bdd
sebarick <157357495+sebarick@users.noreply.github.com>
2024-04-15 14:56:49 +0300

    Create main.js

---
+++ 78/main.js
@@ -0,0 +1,51 @@
+const saveData = () => {
+    const title = document.querySelector("#title").value;
+    const content = document.querySelector("#content").value;
+    const author = document.querySelector("#author").value;
+    const publishDate = document.querySelector("#publishDate").value;
+
+    let storeData = JSON.parse(localStorage.getItem("articles")) || [];
+
+    let userData = {
+        title: title,
+        content: content,
+        author: source,
+        publishDate: publishDate,
+    };
+
+    storeData.push(userData);
+
+    localStorage.setItem("articles", JSON.stringify(storeData));
+    getData();
+}
+
+const getData = () => {
+    let resultData = document.querySelector("#resultData");
+    let storeData = JSON.parse(localStorage.getItem("articles")) || [];
+
+    resultData.innerHTML = "";
+
+    if (storeData && storeData.length > 0) {
+        storeData.forEach((userData, index) => {
+            resultData.innerHTML += `<li>Название: ${userData.title}</li>`;
+            resultData.innerHTML += `<li>Содержание: ${userData.content}</li>`;
+            resultData.innerHTML += `<li>Автор: ${userData.author}</li>`;
+            resultData.innerHTML += `<li>Дата публикации: ${userData.publishDate}</li>`;
+            resultData.innerHTML += `<button onclick="deleteData(${index})">Удалить</button>`;
+            resultData.innerHTML += `<hr><br>`;
+        });
+    } else {
+        resultData.innerHTML += "<li>Данные не найдены</li>"
+    }
+}
+
+const deleteData = (index) => {
+    let storeData = JSON.parse(localStorage.getItem("articles")) || [];
+    storeData.splice(index, 1);
+    localStorage.setItem("articles", JSON.stringify(storeData));
+    getData();
+}
+
+window.onload = () => {
+    getData();
+}
